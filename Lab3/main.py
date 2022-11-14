82629f084170a05e47fe5101a53e723db322a5d2
IPablo271 <69815580+IPablo271@users.noreply.github.com>
2022-11-14 12:14:20 -0600

    Lab3
    Cambio de gameState

--- Lab3/main.py
+++ Lab3/main.py
@@ -22,11 +22,88 @@ dimCH = height / nyC
 # Estadp de las celdas
 gameState = np.zeros((nxC,nyC))
 
+
+gameState[5,3] = 1
+gameState[5,4] = 1
+gameState[5,5] = 1
+
 gameState[21,21] = 1
 gameState[22,22] = 1
 gameState[22,23] = 1
 gameState[21,23] = 1
 gameState[20,23] = 1
+
+
+gameState[41,41] = 1
+gameState[42,42] = 1
+gameState[42,43] = 1
+gameState[41,43] = 1
+gameState[40,43] = 1
+
+gameState[20,10] = 1
+gameState[20,11] = 1
+gameState[20,12] = 1
+
+gameState[10,10] = 1
+gameState[11,11] = 1
+gameState[12,13] = 1
+gameState[11,13] = 1
+gameState[10,13] = 1
+
+
+
+gameState[10,40] = 1
+gameState[11,41] = 1
+gameState[12,43] = 1
+gameState[11,43] = 1
+gameState[10,43] = 1
+
+gameState[30,30] = 1
+gameState[32,32] = 1
+gameState[32,33] = 1
+gameState[31,33] = 1
+gameState[30,33] = 1
+
+gameState[34,38] = 1
+gameState[34,39] = 1
+gameState[34,40] = 1
+
+
+
+gameState[21,21] = 1
+gameState[22,22] = 1
+gameState[22,23] = 1
+gameState[21,23] = 1
+gameState[20,23] = 1
+
+gameState[47,47] = 1
+gameState[48,48] = 1
+gameState[48,49] = 1
+gameState[47,49] = 1
+gameState[47,49] = 1
+
+gameState[43,43] = 1
+gameState[44,44] = 1
+gameState[44,45] = 1
+gameState[43,46] = 1
+gameState[44,46] = 1
+
+
+
+gameState[43,43] = 1
+gameState[28,35] = 1
+gameState[17,17] = 1
+gameState[17,21] = 1
+gameState[48,10] = 1
+
+
+gameState[29,27] = 1
+gameState[29,28] = 1
+gameState[29,29] = 1
+
+
+
+
 
 
 while True:
